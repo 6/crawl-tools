32f29a3daee05d84122b862ff050473599d52eac
Peter Graham <pg.sners@gmail.com>
2012-05-15 12:51:57 -0400

    wrapper for webkit2png for taking website screenshots

--- constants.py
+++ constants.py
@@ -4,4 +4,5 @@ DATA_PATH = "data"
 DATA_PATH_ALEXA = "{0}/alexa/topsites.zip".format(DATA_PATH)
 DATA_PATH_ALEXA_CSV = "{0}/alexa/{1}".format(DATA_PATH, ALEXA_CSV_FILE)
 DATA_PATH_FAVICON = "{0}/favicon/{{0}}.png".format(DATA_PATH)
+DATA_PATH_SCREENSHOT = "{0}/screenshot/".format(DATA_PATH)
 USER_AGENT = "Mozilla/5.0 (compatible; PyCrawl/0.1)"
---
+++ screenshot.py
@@ -0,0 +1,11 @@
+# Wrapper for lib/webkit2png.py for taking screenshots of websites
+import constants
+import subprocess
+
+def screenshot(url, fname=None, width=1024, height=600, delay=1):
+  name_fmt = "" if fname is None else " --filename={0}".format(fname)
+  cmd = "python lib/webkit2png.py {0} -W {1} -H {2} --fullsize --delay={3}{4} --dir={5}".format(url, width, height, delay, name_fmt, constants.DATA_PATH_SCREENSHOT)
+  subprocess.call(cmd, shell=True)
+
+if __name__=="__main__":
+  screenshot("http://www.google.jp", "google.jp")
