f5a9b853a86fb94dc3355be0d2077dbf55c9f689
Peter Graham <pg.sners@gmail.com>
2012-05-15 11:52:02 -0400

    unzip alexa file too

--- alexafetch.py
+++ alexafetch.py
@@ -1,11 +1,17 @@
 import constants
 import pycrawl
+import zipfile
 
 def fetch():
   pc = pycrawl.PyCrawl(constants.USER_AGENT)
   error = pc.download(constants.ALEXA_URL, constants.DATA_PATH_ALEXA)
   if error is not None:
     print "====\nERROR:\n", error
+    return
+  zip = zipfile.ZipFile(constants.DATA_PATH_ALEXA, "r")
+  data = zip.read(constants.ALEXA_CSV_FILE)
+  with open(constants.DATA_PATH_ALEXA_CSV, 'wb') as f:
+    f.write(data)
 
 if __name__=="__main__":
   fetch()
--- constants.py
+++ constants.py
@@ -1,5 +1,7 @@
 ALEXA_URL = "http://s3.amazonaws.com/alexa-static/top-1m.csv.zip"
+ALEXA_CSV_FILE = "top-1m.csv"
 DATA_PATH = "data"
 DATA_PATH_ALEXA = "{0}/alexa/topsites.zip".format(DATA_PATH)
+DATA_PATH_ALEXA_CSV = "{0}/alexa/{1}".format(DATA_PATH, ALEXA_CSV_FILE)
 DATA_PATH_FAVICON = "{0}/favicon/{{0}}.png".format(DATA_PATH)
 USER_AGENT = "Mozilla/5.0 (compatible; PyCrawl/0.1)"
