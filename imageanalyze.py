46882ff6b5701213ff90fd50d303c46d40f7709e
Peter Graham <pg.sners@gmail.com>
2012-05-16 21:30:00 -0400

    color palette detection wrapper

---
+++ imageanalyze.py
@@ -0,0 +1,25 @@
+import numpy
+from PIL import Image
+import constants
+import gitmodules
+gitmodules.add_vendor()
+import colorific
+
+def numpy_array_of(image_path):
+  return numpy.array(Image.open(image_path))
+
+# n_quantized: start with an adaptive palette of this size
+# min_distance: min distance to consider two colors different
+# min_prominence: ignore if less than this proportion of image
+# min_saturation: ignore if not saturated enough
+# max_colors: keep only this many colors
+def palette(image_path, min_saturation=0.05, min_prominence=0.01,
+              min_distance=10.0, max_colors=5, n_quantized=100):
+  colorp = colorific.extract_colors(image_path, min_saturation=min_saturation,
+    min_prominence=min_prominence, min_distance=min_distance,
+    max_colors=max_colors, n_quantized=n_quantized)
+  return [colorific.rgb_to_hex(c.value) for c in colorp.colors]
+
+if __name__=="__main__":
+  print numpy_array_of(constants.DATA_PATH_SCREENSHOT_FILE.format("google.jp"))
+  print palette(constants.DATA_PATH_FAVICON.format("www.google.com"))
