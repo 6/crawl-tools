5ad96847575f8c1e36bc856a8878299a4138b369
Peter Graham <pg.sners@gmail.com>
2012-05-16 21:51:18 -0400

    use submodule for webkit2png

--- screenshot.py
+++ screenshot.py
@@ -4,7 +4,7 @@ import subprocess
 
 def screenshot(url, fname=None, width=1024, height=600, delay=1):
   name_fmt = "" if fname is None else " --filename={0}".format(fname)
-  cmd = "python vendor/webkit2png.py {0} -W {1} -H {2} --fullsize --delay={3}{4} --dir={5}".format(url, width, height, delay, name_fmt, constants.DATA_PATH_SCREENSHOT)
+  cmd = "python vendor/webkit2png/webkit2png {0} -W {1} -H {2} --fullsize --delay={3}{4} --dir={5}".format(url, width, height, delay, name_fmt, constants.DATA_PATH_SCREENSHOT)
   subprocess.call(cmd, shell=True)
 
 if __name__=="__main__":
