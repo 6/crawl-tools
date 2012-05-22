c8db16a267506fd97b8509dfda41f9ffe2c5be51
Peter Graham <pg.sners@gmail.com>
2012-05-21 21:53:48 -0400

    `dir` is a built in function - rename

--- gitmodules.py
+++ gitmodules.py
@@ -4,7 +4,7 @@ import sys
 # based on: http://stackoverflow.com/a/5137737/223408
 def add_vendor():
   vendor = 'vendor'
-  for dir in os.listdir(vendor):
-    path = os.path.join(vendor, dir)
+  for directory in os.listdir(vendor):
+    path = os.path.join(vendor, directory)
     if not path in sys.path:
       sys.path.append(path)
