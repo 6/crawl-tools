14c55c3b9e55f28880131a2083ec21a58940d1c4
Peter Graham <pg.sners@gmail.com>
2012-05-21 23:04:44 -0400

    change default screenshot delay to 0

--- screenshot.py
+++ screenshot.py
@@ -2,7 +2,7 @@
 import constants
 import subprocess
 
-def screenshot(url, fname=None, width=1024, height=600, delay=1):
+def screenshot(url, fname=None, width=1024, height=600, delay=0):
   name_fmt = "" if fname is None else " --filename={0}".format(fname)
   cmd = "python vendor/webkit2png/webkit2png {0} -W {1} -H {2} --fullsize --delay={3}{4} --dir={5}".format(url, width, height, delay, name_fmt, constants.DATA_PATH_SCREENSHOT)
   subprocess.call(cmd, shell=True)
